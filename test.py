ddd24039956c7b518a55173891ca729f97cd6a6f
听雪 <38068739+fgfxf@users.noreply.github.com>
2021-08-24 10:53:59 +0800

    Update test.py

--- test.py
+++ test.py
@@ -5,9 +5,11 @@ import socks
 def setProxies():
     urlapi="http://tiqu.pyhttp.taolop.com/getip?count=1&neek=8737&type=1&yys=0&port=2&sb=&mr=1&sep=1&city=411200&time=2"
     proIp=requests.get(url=urlapi).text
+    print(proIp)
     proIp=proIp.split(':')
     Ip=proIp[0]
     Port=proIp[1]
+    print(proIp,Ip,Port)
     socks.set_default_proxy(socks.SOCKS5, Ip, int(Port))
     socket.socket = socks.socksocket
     return 1
